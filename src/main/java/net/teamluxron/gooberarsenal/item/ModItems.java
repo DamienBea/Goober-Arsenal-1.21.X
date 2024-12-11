6032bf01cea4ba2a8181bff46b24f097f1690b98
DamienBea <Damienbeacume@gmail.com>
2024-12-11 13:32:11 -0500

    Goober Arsenal Fabric 1.21.X

---
+++ src/main/java/net/teamluxron/gooberarsenal/item/ModItems.java
@@ -0,0 +1,29 @@
+package net.teamluxron.gooberarsenal.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.teamluxron.gooberarsenal.GooberArsenal;
+
+public class ModItems {
+
+    //Crafting Ingredients
+    public static final Item OBSIDIAN_HILT = registerItem("obsidian_hilt", new Item(new Item.Settings()));
+    public static final Item OBSIDIAN_HANDGUARD = registerItem("obsidian_handguard", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(GooberArsenal.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        GooberArsenal.LOGGER.info("Registering Mod Items for " + GooberArsenal.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(OBSIDIAN_HILT);
+            fabricItemGroupEntries.add(OBSIDIAN_HANDGUARD);
+        });
+    }
+}
